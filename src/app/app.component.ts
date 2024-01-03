c31df87046de4def032d25c594f31a5bf451886a
Saskia Steensma <saskia.steensma@rhdhv.com>
2024-01-03 23:50:56 +0100

    Add the httpClientModule.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
@@ -6,7 +7,7 @@ import { SignupComponent } from './components/signup/signup.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignupComponent],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
